7d7377025a3c2c1c98f87d4b39512388a1bdaf32
erica <erica.depetrillo@mail.mcgill.ca>
2023-12-02 18:01:25 -0500

    umple extraction works although not perfect: extra things are being added to myfeaturelist + no multiplicity yet

--- my.xtext.featurelist.myfeaturelist/src/domainModelManipulations/ExtractClassesFromDomainModel.java
+++ my.xtext.featurelist.myfeaturelist/src/domainModelManipulations/ExtractClassesFromDomainModel.java
@@ -1,7 +1,11 @@
 package domainModelManipulations;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Collections;
+import java.util.Scanner;
 
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.common.util.URI;
@@ -17,16 +21,24 @@ import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.emf.ecore.xmi.impl.EcoreResourceFactoryImpl;
+import org.eclipse.xtext.resource.XtextResourceSet;
+
+import com.google.inject.Injector;
+
+import my.xtext.featurelist.myfeaturelist.MyFeatureListStandaloneSetup;
 
 public class ExtractClassesFromDomainModel {
-	
+
 	public static void main(String[] args) {
+
+		String result = "";
+
 		// Create a resource set.
 		ResourceSet resourceSet = new ResourceSetImpl();
 
 		// Register the default resource factory -- only needed for stand-alone!
 		resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put(
-		"ecore", new EcoreResourceFactoryImpl());
+				"ecore", new EcoreResourceFactoryImpl());
 
 		// Register the package -- only needed for stand-alone!
 		EcorePackage ecorePackage = EcorePackage.eINSTANCE;
@@ -36,39 +48,68 @@ public class ExtractClassesFromDomainModel {
 		// Demand load the resource for this file.
 		Resource resource = resourceSet.getResource(fileURI, true);
 
-		// Print the contents of the resource to System.out.
-		/*try
-		{
-		resource.save(System.out, Collections.EMPTY_MAP);
-		}
-		catch (IOException e) {
-			System.out.println("problem");
-		}*/
-		
 		//get classes
 		EPackage epackage = (EPackage) resource.getContents().get(0);
-		
+
 		for (EClassifier classifier : epackage.getEClassifiers()) {
 			if (classifier instanceof EClass) {
-				System.out.println(classifier.getName());
+				result = result.concat("concept " + classifier.getName() + "\n");
 				for (EStructuralFeature feature : ((EClass) classifier).getEStructuralFeatures()) {
 					if (feature instanceof EAttribute) {
-						System.out.println(feature.getName());
+						String featureType = getJavaType(feature);
+						result = result.concat("\t" + featureType + " " + feature.getName() + "\n");
 					}
 					if (feature instanceof EReference) {
-						
+						result = result.concat("\t" + feature.getEType().getName() + " " + feature.getName() + "\n");
 					}
 				}
+				result = result.concat("\n");
 			}
-			System.out.println();
 		}
-		
-		
-		
-		/*for (EObject object : objects) {
-			if (object.getClass() == EClassifier.class) {
-				System.out.println(object.toString());
+
+		//System.out.println(result);
+
+		printToMyFeatureListFile(result);
+	}
+
+	private static String getJavaType(EStructuralFeature feature) {
+		if (feature.getEType().getName() == "EString") {
+			return "String";
+		}
+		return feature.getEType().getInstanceClassName();
+	}
+
+	private static void printToMyFeatureListFile(String result) {
+		String xtextFileContent = "";
+
+		//save info from current xtext (if any)
+		try {
+			File myObj = new File("C:\\Users\\erica\\Documents\\UNI\\Grad School\\Thesis\\grad-project\\restaruantapp\\src\\myrestaurantapp.myfeaturelist");
+			Scanner myReader = new Scanner(myObj);
+			boolean flag = false;
+			while (myReader.hasNextLine()) {
+				String data = myReader.nextLine();
+				if (data.equals("constraints")) {
+					flag = true;
+				}
+				if (flag) {
+					xtextFileContent = xtextFileContent.concat(data + "\n");
+				}
 			}
-		}*/
+			myReader.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("File does not exist.");
+		}
+
+		try {
+			//TODO: CHANGE TO SAME FILE NAME AS LINE 95 WHEN EXTRACTION WILL BE BETTER
+			FileWriter myWriter = new FileWriter("C:\\Users\\erica\\Documents\\UNI\\Grad School\\Thesis\\grad-project\\myrestaurantappFromUmple.myfeaturelist");
+			myWriter.write(result);
+			myWriter.close();
+			System.out.println("MyFeatureList was updated.");
+		} catch (IOException e) {
+			System.out.println("Writing to MyFeatureList failed.");
+			e.printStackTrace();
+		}
 	}
 }
